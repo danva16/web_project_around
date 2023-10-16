03bdb6c2dc2e68ca90c9d249fe9ccd4b1a40390f
Danva16 <danieladannava111116@gmail.com>
2023-10-16 12:08:47 -0600

    feater: creacion de la clase Section

---
+++ script/Section.js
@@ -0,0 +1,18 @@
+class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  //Método público que renderiza todos los elementos de la página
+  rendererItems() {
+    this._items.forEach(item => {
+      this._renderer(item);
+    });
+  }
+
+  addItem(element) {
+    this._container.prepend(element);
+  }
+}
