da4e52f744a422bd843c5570f498569c8a745fb9
Danva16 <danieladannava111116@gmail.com>
2023-10-19 02:39:09 -0600

    exportacion de constantes

--- script/constants.js
+++ script/constants.js
@@ -58,3 +58,7 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
   }
 ];
+
+export { cardTemplateSelector, popupElement, placesElement, profileFormElement, imageFormElement, nameElement, employmentElement
+,nameInput, employmentInput, formElement, buttonEdit, buttonAdd, closeButtons, submitButtonProfile, submitButtonImage
+, profileFormConfig, imageFormConfig, initialCards };
