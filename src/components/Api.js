0ae072160704206cff946e35bfce2cad0cee77df
Danva16 <danieladannava111116@gmail.com>
2023-11-16 01:53:01 -0600

    creacion de clase API

---
+++ src/components/Api.js
@@ -0,0 +1,18 @@
+class Api {
+  constructor(options) {
+    this.baseUrl = options.baseUrl;
+    this.headers = options.headers;
+  }
+
+  getInitialCards() {
+    return fetch(`${this.baseUrl}/cards`, {
+      headers: this.headers
+    })
+    .then(res => {
+      if(res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Error: ${res.status}`);
+    })
+  }
+}
